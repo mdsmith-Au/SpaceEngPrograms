4c85fc79656e27943f1bbde3317c5a4743af2ee4
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-02-10 23:08:58 -0500

    An automatic door opener for an entire base with only a rotor, 1-4 sensors and a programming block (+ timer block, optional)

--- AutoDoor.cs
+++ AutoDoor.cs
@@ -12,26 +12,83 @@ class AutoDoor : MyGridProgram
     // Copy starting here and ignore last } - programming block does not need above class def'n
 
     // Version 1.0
-    // Author: cptmds 2016-02-08
+    // Author: cptmds 2016-02-10
+
+
+    // User defined variables
+    private const String PREFIX = "[AUTODOOR]";
+    private const double triggerDist = 4;
+
+    // Sensor range values
+    // Set within in-game limits which are as of 2016-02-10 min 1 and max 50
+    private const float SensorLeft = 30f;
+    private const float SensorRight = 30f;
+    private const float SensorTop = 1f;
+    private const float SensorBottom = 50f;
+    private const float SensorFront = 50f;
+    private const float SensorBack = 1f;
+
+    // Speed at which the rotor should run at
+    // Faster speeds = faster door response
+    private const float rotorSpeed = 30f;
+
 
     private AutoDoorProgram autodoor = null;
 
     void Main(string argument)
     {
-        autodoor = new AutoDoorProgram(GridTerminalSystem, Me, Echo, ElapsedTime);
+        if (autodoor == null) autodoor = new AutoDoorProgram(GridTerminalSystem, Me, Echo, ElapsedTime);
 
+        autodoor.playerDetected();
     }
 
 
 
     public class AutoDoorProgram
     {
-        // User defined variables
 
         protected IMyGridTerminalSystem GridTerminalSystem;
         protected Action<string> Echo;
         protected TimeSpan ElapsedTime;
         protected IMyTerminalBlock Me;
+
+        private List<IMySensorBlock> sensors;
+        private IMyMotorStator rotor;
+        List<IMyDoor> doors;
+
+        // Assume this is triggered when sensor detects player
+        public void playerDetected()
+        {
+
+            //Echo("Player detected!");
+
+            for (int i = 0; i < doors.Count; i++)
+            {
+                IMyDoor door = doors[i];
+                Boolean anyoneNearDoor = false;
+                for (int j = 0; j < sensors.Count; j++)
+                {
+                    VRage.ModAPI.IMyEntity player = sensors[j].LastDetectedEntity;
+                    if (player == null) continue;
+                    //Echo("Checking player at " + player.GetPosition().ToString() + " against door " + door.CustomName);
+
+                    if ((player.GetPosition() - door.GetPosition()).Length() <= triggerDist)
+                    {
+                        anyoneNearDoor = true;
+                        break;
+                    }
+                }
+                if (anyoneNearDoor)
+                {
+                    door.ApplyAction("Open_On");
+                }
+                else
+                {
+                    door.ApplyAction("Open_Off");
+                }
+            }
+        }
+
 
 
         public AutoDoorProgram(IMyGridTerminalSystem grid, IMyProgrammableBlock me, Action<string> echo, TimeSpan elapsedTime)
@@ -41,28 +98,71 @@ class AutoDoor : MyGridProgram
             ElapsedTime = elapsedTime;
             Me = me;
 
-            IMyDoor door = GridTerminalSystem.GetBlockWithName("TestDoor") as IMyDoor;
+            doors = new List<IMyDoor>();
+            sensors = new List<IMySensorBlock>();
 
-            IMySensorBlock sensor = GridTerminalSystem.GetBlockWithName("TestSensor") as IMySensorBlock;
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
 
-            //Echo("Door position: " + door.GetPosition().ToString());
+            grid.SearchBlocksOfName(PREFIX, blocks);
 
-            VRage.ModAPI.IMyEntity player = sensor.LastDetectedEntity;
+            // Add some error handling for blocks not found
 
-            //Echo("Sensor thinks: " + player.GetPosition().ToString());
-
-            double dist = (player.GetPosition() - door.GetPosition()).Length() ;
-
-            Echo("Distance: " + dist.ToString());
-
-            if (dist < 3)
+            for (int i = 0; i < blocks.Count; i++)
             {
-                door.ApplyAction("Open_On");
+                IMyTerminalBlock block = blocks[i];
+                String blockType = block.DefinitionDisplayNameText;
+                String blockName = block.CustomName;
+
+                //Echo("Processing block " + blockName);
+
+                if (blockType.Equals("Sensor"))
+                {
+                    IMySensorBlock sensor = block as IMySensorBlock;
+                    sensor.ApplyAction("OnOff_On");
+
+                    List<ITerminalProperty> properties = new List<ITerminalProperty>();
+                    sensor.GetProperties(properties);
+
+                    sensor.SetValueFloat("Back", SensorBack);
+                    sensor.SetValueFloat("Bottom", SensorBottom);
+                    sensor.SetValueFloat("Top", SensorTop);
+                    sensor.SetValueFloat("Left", SensorLeft);
+                    sensor.SetValueFloat("Right", SensorRight);
+                    sensor.SetValueFloat("Front", SensorFront);
+                    sensor.SetValueBool("Detect Asteroids", false);
+                    sensor.SetValueBool("Detect Enemy", false);
+                    sensor.SetValueBool("Detect Floating Objects", false);
+                    sensor.SetValueBool("Detect Friendly", true);
+                    sensor.SetValueBool("Detect Large Ships", false);
+                    sensor.SetValueBool("Detect Neutral", false);
+                    sensor.SetValueBool("Detect Owner", true);
+                    sensor.SetValueBool("Detect Players", true);
+                    sensor.SetValueBool("Detect Small Ships", false);
+                    sensor.SetValueBool("Detect Stations", false);
+                    sensor.SetValueBool("Audible Proximity Alert", false);
+                    sensors.Add(sensor);
+
+                }
+                else if (blockType.Equals("Sliding Door") || blockType.Equals("Door"))
+                {
+                    IMyDoor door = block as IMyDoor;
+                    door.ApplyAction("Open_Off");
+                    doors.Add(door);
+                }
+                else if (blockType.Equals("Rotor") || blockType.Equals("Advanced Rotor"))
+                {
+                    rotor = block as IMyMotorStator;
+                    rotor.ApplyAction("OnOff_On");
+                    rotor.SetValueFloat("Torque", 3.36E+07f);
+                    rotor.SetValueFloat("BrakingTorque", 3.36E+07f);
+                    rotor.SetValueFloat("Velocity", rotorSpeed);
+                    rotor.SetValueFloat("UpperLimit", float.PositiveInfinity);
+                    rotor.SetValueFloat("LowerLimit", float.NegativeInfinity);
+
+                    // Add config here
+                }
             }
-            else
-            {
-                door.ApplyAction("Open_Off");
-            }
+
 
         }
     }
---
+++ Elevator2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using VRageMath;
+
+class Elevator2 : MyGridProgram
+{
+
+    // Copy starting here and ignore last } - programming block does not need above class def'n
+
+    // Version 1.0
+    // Author: cptmds 2016-02-10
+
+
+    private Elevator elev = null;
+
+    void Main(string argument)
+    {
+        if (elev == null) elev = new Elevator(GridTerminalSystem, Me, Echo, ElapsedTime);
+
+        if (argument.Length > 0)
+        {
+
+        }
+        elev.mainLoop();
+    }
+
+
+
+    public class Elevator
+    {
+        // User defined variables
+
+        private const string ELEVATOR_SUFFIX = "[ELEV]"; // What all elevator related blocks should have in their name
+        private const float epsilon = 0.2f; // How close the piston must be (in meters) to a floor to be considered at that floor
+        private const float elevatorSpeed = 2f; // Speed of the elevator in m/s
+
+        private float[] floorLocations;
+
+
+        protected IMyGridTerminalSystem GridTerminalSystem;
+        protected Action<string> Echo;
+        protected TimeSpan ElapsedTime;
+        protected IMyTerminalBlock Me;
+
+        private IMySoundBlock sound;
+        private List<IMyPistonBase> pistons;
+        private IMyDoor cabinDoor;
+        private List
+
+       
+
+        public Elevator(IMyGridTerminalSystem grid, IMyProgrammableBlock me, Action<string> echo, TimeSpan elapsedTime)
+        {
+            GridTerminalSystem = grid;
+            Echo = echo;
+            ElapsedTime = elapsedTime;
+            Me = me;
+
+        }
+
+        // Process all requests - main thread (think while(true) in a microprocessor)
+        // Note this assume a 1Hz clock cycle from a timer block
+        public void mainLoop()
+        {
+        }
+    }
+
+    // Stop copying here
+}
